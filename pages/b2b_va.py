7915573ef22a8e18f19d450a4c1970ccfb6304f3
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2026-02-09 16:08:04 +0700

    Update b2b_va.py

--- pages/b2b_va.py
+++ pages/b2b_va.py
@@ -6,19 +6,12 @@ st.set_page_config(layout="wide")
 center, right = st.columns([4.5, 1.5])
 
 with right:
-    render_section_sidebar(["Overview", "Use case", "Integration Methods"])
+    render_section_sidebar([
+        "Overview",
+        "Use case",
+        "Integration Methods"
+    ])
 
 with center:
     st.title("Virtual Account")
-
-    st.markdown('<a name="integration-methods"></a>', unsafe_allow_html=True)
-    st.header("Integration Methods")
-
-    st.page_link(
-        page="b2b_va_direct_basic",
-        label="Direct Merchant / Basic"
-    )
-    st.page_link(
-        page="b2b_va_direct_h2h",
-        label="Direct Merchant / H2H"
-    )
+    st.write("VA hub page")
