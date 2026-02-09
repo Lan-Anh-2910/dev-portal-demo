d47ecc7a0abab0b54722f7f21fce2222304feba9
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2026-02-09 16:07:31 +0700

    Update product_sidebar.py

--- components/product_sidebar.py
+++ components/product_sidebar.py
@@ -5,18 +5,21 @@ def render_product_sidebar():
         st.markdown("## Products")
 
         with st.expander("B2B", expanded=True):
-            st.markdown("• Virtual Account")
-            st.markdown("• BNPL")
-            st.markdown("• Installment")
-            st.markdown("• Card Payment")
+            st.page_link(
+                page="b2b_va",
+                label="Virtual Account"
+            )
+            st.markdown("BNPL")
+            st.markdown("Installment")
+            st.markdown("Card Payment")
 
         with st.expander("Bill"):
-            st.markdown("• Bill Payment")
-            st.markdown("• Insurance")
+            st.markdown("Bill Payment")
+            st.markdown("Insurance")
 
         with st.expander("Leadgen"):
-            st.markdown("• Leadgen")
+            st.markdown("Leadgen")
 
         with st.expander("Cross-border"):
-            st.markdown("• Collection")
-            st.markdown("• Disbursement")
+            st.markdown("Collection")
+            st.markdown("Disbursement")
