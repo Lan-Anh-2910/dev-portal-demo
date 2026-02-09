53b8dd587d9e9b6cda11eed18d63bb2480d23aa0
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2026-02-09 16:06:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.set_page_config(layout="wide")
 render_product_sidebar()
 
 st.title("Developer Portal")
-st.write("Select a product to start from the content area.")
+st.write("Choose a product from the left menu")
