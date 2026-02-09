91223c98a88226b992f970b8a46d8dcc764c9bd5
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2026-02-09 17:03:35 +0700

    Create section_config.py

---
+++ components/section_config.py
@@ -0,0 +1,16 @@
+SECTION_CONFIG = {
+    "va_overview": [
+        "Overview",
+        "Use case",
+        "Integration Methods"
+    ],
+    "integration_detail": [
+        "Overview",
+        "Flow",
+        "Sandbox",
+        "API Reference",
+        "Security",
+        "Webhook",
+        "Error Codes"
+    ]
+}
